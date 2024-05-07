a538412e49837beb93284faf1a2eaac4b5e4ace4
minsoo.lee <preinpost@gmail.com>
2024-05-08 00:07:21 +0900

    init

---
+++ lib.s
@@ -0,0 +1,4 @@
+	.text
+	.file	"lib.650f6d897e6e283b-cgu.0"
+	.ident	"rustc version 1.75.0 (82e1608df 2023-12-21)"
+	.section	".note.GNU-stack","",@progbits
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use crate::ch1::thread_parking::thread_parking;
+
+mod ch1;
+mod ch5;
+
+fn main() {
+    thread_parking();
+}
