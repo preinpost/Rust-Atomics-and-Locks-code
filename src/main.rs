98f4ea3bd2f0426a1d2120a8900e73463a42896a
minsoo.lee <preinpost@gmail.com>
2024-05-16 23:20:33 +0900

    5장-6

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::ch2::atomic::{example3, example4, fetch_add_example};
+use crate::ch5::channel::ch5_6::run_5_6;
 use crate::ch5::channel::safe_channel::run;
 use crate::ch5::channel::safe_channel_without_arc::run3;
 
@@ -7,5 +8,5 @@ mod ch2;
 mod ch5;
 
 fn main() {
-    run3();
+    run_5_6();
 }
