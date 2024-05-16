079591b134673dc4902980e2b683fb52e746a524
minsoo.lee <preinpost@gmail.com>
2024-05-16 23:01:24 +0900

    5장-5

--- src/ch5/channel.rs
+++ src/ch5/channel.rs
@@ -73,6 +73,12 @@ pub mod safe_channel {
     use std::sync::Arc;
     use std::sync::atomic::AtomicBool;
     use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+    use std::thread;
+
+    struct Channel<T> {
+        message: UnsafeCell<MaybeUninit<T>>,
+        ready: AtomicBool,
+    }
 
     pub struct Sender<T> {
         channel: Arc<Channel<T>>,
@@ -80,11 +86,6 @@ pub mod safe_channel {
 
     pub struct Receiver<T> {
         channel: Arc<Channel<T>>,
-    }
-
-    struct Channel<T> {
-        message: UnsafeCell<MaybeUninit<T>>,
-        ready: AtomicBool,
     }
 
     unsafe impl<T> Sync for Channel<T> where T: Send {}
@@ -123,5 +124,107 @@ pub mod safe_channel {
                 unsafe { self.message.get_mut().assume_init_drop() }
             }
         }
+    }
+
+    pub fn run() {
+        thread::scope(|s| {
+            let (sender, receiver) = channel();
+            let t = thread::current();
+            s.spawn(move || {
+                sender.send("hello world!");
+                t.unpark();
+            });
+            while !receiver.is_ready() {
+                thread::park();
+            }
+
+            println!("{}", receiver.receive());
+            // assert_eq!(receiver.receive(), "hello world!");
+        });
+    }
+}
+
+
+pub mod safe_channel_without_arc {
+    use std::cell::UnsafeCell;
+    use std::mem::MaybeUninit;
+    use std::sync::atomic::AtomicBool;
+    use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+    use std::thread;
+    use crate::ch5::channel;
+
+    pub struct Channel<T> {
+        message: UnsafeCell<MaybeUninit<T>>,
+        ready: AtomicBool,
+    }
+
+    unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+    pub struct Sender<'a, T> {
+        channel: &'a Channel<T>,
+    }
+
+    pub struct Receiver<'a, T> {
+        channel: &'a Channel<T>,
+    }
+
+    impl<T> Channel<T> {
+        pub const fn new() -> Self {
+            Self {
+                message: UnsafeCell::new(MaybeUninit::uninit()),
+                ready: AtomicBool::new(false),
+            }
+        }
+
+        pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
+            *self = Self::new();
+            (Sender { channel: self }, Receiver { channel: self })
+        }
+    }
+
+    impl<T> Sender<'_, T> {
+        pub fn send(self, message: T) {
+            unsafe { (*self.channel.message.get()).write(message) };
+            self.channel.ready.store(true, Release);
+        }
+    }
+
+    impl<T> Receiver<'_, T> {
+        pub fn is_ready(&self) -> bool {
+            self.channel.ready.load(Relaxed)
+        }
+
+        pub fn receive(self) -> T {
+            if !self.channel.ready.swap(false, Acquire) {
+                panic!("no message available!");
+            }
+            unsafe { (*self.channel.message.get()).assume_init_read() }
+        }
+    }
+
+    impl<T> Drop for Channel<T> {
+        fn drop(&mut self) {
+            if *self.ready.get_mut() {
+                unsafe { self.message.get_mut().assume_init_drop() }
+            }
+        }
+    }
+
+    pub fn run3() {
+        let mut channel = Channel::new();
+        thread::scope(|s| {
+            let (sender, receiver) = channel.split();
+            let t = thread::current();
+            s.spawn(move || {
+                sender.send("hello world!");
+                t.unpark();
+            });
+            while !receiver.is_ready() {
+                thread::park();
+            }
+
+            assert_eq!(receiver.receive(), "hello world!");
+
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use crate::ch2::atomic::{example3, example4, fetch_add_example};
+use crate::ch5::channel::safe_channel::run;
+use crate::ch5::channel::safe_channel_without_arc::run3;
 
 mod ch1;
 mod ch2;
 mod ch5;
 
 fn main() {
-    example4();
+    run3();
 }
